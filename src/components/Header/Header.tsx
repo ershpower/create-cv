e54d80b249f36432a46576d6b5c3d569f6517c91
INNO\VDErshov <VDErshov@inno.tech>
2024-01-12 11:05:18 +0400

    header and fix

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import * as S from './style';
+
+const Header = () => {
+    return (
+        <S.Header>
+            <h1>Create CV</h1>
+            <p>by Vlad Ershov</p>
+        </S.Header>
+    );
+};
+
+export { Header };
---
+++ src/components/Header/style.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+    margin: 0 auto;
+    font-size: 20px;
+    max-width: 80%;
+    background: #3a606e;
+    padding: 30px 50px;
+    border-bottom-left-radius: 15px;
+    border-bottom-right-radius: 15px;
+    color: white;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
